6ec111bd0911ebebde94ca09f2f96697cccad882
Er1ck-Esp1n0sa <zs20007628@estudiantes.uv.mx>
2023-10-23 09:01:29 -0600

    add 3a api

--- index.js
+++ index.js
@@ -37,6 +37,22 @@ app.get('/hours', (req, res, next) =>{
 	res.sendfile("assets/1a_dev_hours.json");
 })
 
+app.get('/3a1', (req, res, next) => {
+	res.sendfile("assets/3a_test1.json");
+});
+
+app.get('/3a2', (req, res, next) => {
+	res.sendfile("assets/3a_test2.json");
+});
+
+app.get('/3a3', (req, res, next) => {
+	res.sendfile("assets/3a_test3.json");
+});
+
+app.get('/3a4', (req, res, next) => {
+	res.sendfile("assets/3a_test4.json");
+});
+
 app.listen(port,  () => 
 	console.log('listening on http://localhost:8080/'
 ));
